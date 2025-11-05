003de7af04b3b0dff3de344d6288b50ce67a2214
Igor Nunes Patricio <igornunespatricio@gmail.com>
2025-11-04 22:51:10 -0300

    add trigger refresh of materialized view northwind db

--- example_sql_scripts/northwind_db_examples/trigger_materialized_view_refresh.sql
+++ example_sql_scripts/northwind_db_examples/trigger_materialized_view_refresh.sql
@@ -1,56 +1,13 @@
 SELECT *
-FROM mv_monthly_revenue;
+FROM mv_monthly_revenue
+ORDER BY YEAR DESC,
+    MONTH DESC;
 
-INSERT INTO orders
-VALUES (
-        11075,
-        'RICSU',
-        8,
-        '1999-05-06',
-        '1999-06-03',
-        NULL,
-        2,
-        6.19000006,
-        'Richter Supermarkt',
-        'Starenweg 5',
-        'Genève',
-        NULL,
-        '1204',
-        'Switzerland'
-    );
-
-INSERT INTO orders
-VALUES (
-        11076,
-        'BONAP',
-        4,
-        '1999-05-06',
-        '1999-06-03',
-        NULL,
-        2,
-        38.2799988,
-        'Bon app''',
-        '12, rue des Bouchers',
-        'Marseille',
-        NULL,
-        '13008',
-        'France'
-    );
-
-INSERT INTO orders
-VALUES (
-        11077,
-        'RATTC',
-        1,
-        '1999-05-06',
-        '1999-06-03',
-        NULL,
-        2,
-        8.52999973,
-        'Rattlesnake Canyon Grocery',
-        '2817 Milton Dr.',
-        'Albuquerque',
-        'NM',
-        '87110',
-        'USA'
+UPDATE orders
+SET order_date = '1999-05-01'
+WHERE order_id IN (
+        SELECT order_id
+        FROM orders
+        ORDER BY order_date DESC
+        LIMIT 10
     );
